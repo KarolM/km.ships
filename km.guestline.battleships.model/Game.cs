abece00cc38d923c6cf93b606c6c0fbd98dda94f
KarolM <karol.minczynski@br-ag.eu>
2020-12-19 11:12:41 +0100

    Gameplaye initiaiton

---
+++ km.guestline.battleships.model/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace km.guestline.battleships.model
+{
+    public class Game
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        public Player NextMoveBy { get; set; }
+
+        public Game(Player player1, Player player2, StartingPlayerEnum starter = StartingPlayerEnum.First)
+        {
+            this.Player1 = player1;
+            this.Player2 = player2;
+
+            switch (starter)
+            {
+                case StartingPlayerEnum.First:
+                    NextMoveBy = player1;
+                case StartingPlayerEnum.Second:
+                    NextMoveBy = player2;
+
+            }
+        }
+
+        public enum StartingPlayerEnum
+        {
+            First, 
+            Second
+        }
+    }
+}
---
+++ km.guestline.battleships.model/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace km.guestline.battleships.model
+{
+    class Player
+    {
+        public Grid PlayerGrid { get; set; }
+    }
+}
