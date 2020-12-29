6719d3324686a4a4d229a5164d7e6e443a6071c4
KarolM <karol.minczynski@br-ag.eu>
2020-12-29 15:08:13 +0100

    Grid size intialize

---
+++ km.guestline.battleships.modelTests/GameTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using km.guestline.battleships.model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace km.guestline.battleships.model.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void GameTest()
+        {
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game g = new Game(p1, p2, Game.StartingPlayerEnum.Player1);
+            Assert.AreEqual(g.NextMoveBy, p1);
+            g = new Game(p1, p2, Game.StartingPlayerEnum.Player2);
+            Assert.AreEqual(g.NextMoveBy, p2);
+        }
+    }
+}
--- km.guestline.battleships.modelTests/GridTests.cs
+++ km.guestline.battleships.modelTests/GridTests.cs
@@ -31,5 +31,33 @@ namespace km.guestline.battleships.Tests
             Assert.AreEqual('A', s.Column);
             Assert.AreEqual(1, s.Row);
         }
+        
+        [TestMethod()]
+        public void GridReference()
+        {
+            Grid b = new Grid();
+            Square s = b['A', 1];
+            Assert.AreEqual(b['A', 1], s);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException), "Cannot find square with address Z1000")]
+        public void GridIncorrectAddress()
+        {
+            Grid b = new Grid();
+            Square s = b['Z', 1000];
+            Assert.Fail("It should never get here");
+        }
+
+        [TestMethod()]
+        public void GridUntytpicalSize()
+        {
+            Grid b = new Grid(20, 20);
+            Assert.AreEqual(20, b.colSize);
+            Assert.AreEqual(20, b.rowSize);
+            Square s = b['A', 20];
+            Assert.AreEqual('A', s.Column);
+            Assert.AreEqual(20, s.Row);
+        }
     }
 }
